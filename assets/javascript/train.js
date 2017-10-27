005adc7ffa7830ef94eda9faf4de388dca755f70
maggiebarker <maggieb.06@gmail.com>
2017-10-26 20:47:26 -0600

    linked in firebase

---
+++ assets/javascript/train.js
@@ -0,0 +1,12 @@
+//Initialize firebase
+  var config = {
+    apiKey: "AIzaSyAN1hURw7UYWY0wCucJM8gjHKpMoeG6NFQ",
+    authDomain: "traintime-e14f7.firebaseapp.com",
+    databaseURL: "https://traintime-e14f7.firebaseio.com",
+    projectId: "traintime-e14f7",
+    storageBucket: "",
+    messagingSenderId: "360980975407"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
